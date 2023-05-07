b72185bef252f1ab7650760285b39ef00b003e5e
josehm <josehm1999@gmail.com>
2023-05-07 16:27:43 -0500

    Chore: CSS changes in PriceFilter component

--- src/components/PriceFilter.tsx
+++ src/components/PriceFilter.tsx
@@ -1,45 +1,54 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent } from 'react';
 
 export default function PriceFilter() {
-  const [minPrice, setMinPrice] = useState('');
-  const [maxPrice, setMaxPrice] = useState('');
+	const [minPrice, setMinPrice] = useState('');
+	const [maxPrice, setMaxPrice] = useState('');
 
-  const handleMinPriceChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setMinPrice(event.target.value);
-  };
+	const handleMinPriceChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setMinPrice(event.target.value);
+	};
 
-  const handleMaxPriceChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setMaxPrice(event.target.value);
-  };
+	const handleMaxPriceChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setMaxPrice(event.target.value);
+	};
 
-  const handleFilterClick = () => {
-    console.log(
-      `Filter items with minPrice ${minPrice} and maxPrice ${maxPrice}`
-    );
-  };
-  return (
-    <div className=''>
-      <div className='input-group'>
-        <input
-          type='number'
-          id='min-price'
-          value={minPrice}
-          onChange={handleMinPriceChange}
-          placeholder='$ Min'
-          min={'0'}
-        />
-      </div>
-      <div className='input-group'>
-        <input
-          type='number'
-          id='max-price'
-          value={maxPrice}
-          onChange={handleMaxPriceChange}
-          placeholder='$ Max'
-          min={'0'}
-        />
-      </div>
-      <button onClick={handleFilterClick}>Filter</button>
-    </div>
-  );
+	const handleFilterClick = () => {
+		console.log(
+			`Filter items with minPrice ${minPrice} and maxPrice ${maxPrice}`
+		);
+	};
+
+	return (
+		<div className='p-5 grid '>
+			<form action=''>
+				<div className='input-filters'>
+					<div className='input-group'>
+						<input
+							type='text'
+							inputMode='numeric'
+							id='min-price'
+							value={minPrice}
+							onChange={handleMinPriceChange}
+							placeholder='$ Min'
+							min={'0'}
+						/>
+					</div>
+					<div className='input-group'>
+						<input
+							type='text'
+							inputMode='numeric'
+							id='max-price'
+							value={maxPrice}
+							onChange={handleMaxPriceChange}
+							placeholder='$ Max'
+							min={'0'}
+						/>
+					</div>
+				</div>
+				<div>
+					<button onClick={handleFilterClick}>Filter</button>
+				</div>
+			</form>
+		</div>
+	);
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,127 +4,130 @@ import { FaChevronDown } from 'react-icons/fa';
 import PriceFilter from './PriceFilter';
 
 export default function Navbar({ children }: { children: ReactNode }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isExpandedEstadoPropiedad, setIsExpandedEstadoPropiedad] =
-    useState(false);
-  const [isExpandedPrecio, setIsExpandedPrecio] = useState(false);
-  const [isExpandedCategoria, setIsExpandedCategoria] = useState(false);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	const [isExpandedEstadoPropiedad, setIsExpandedEstadoPropiedad] =
+		useState(false);
+	const [isExpandedPrecio, setIsExpandedPrecio] = useState(false);
+	const [isExpandedCategoria, setIsExpandedCategoria] = useState(false);
 
+	const handleSidebarToggle = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
 
-  const handleSidebarToggle = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+	const drawer_styles = `${
+		isSidebarOpen
+			? 'drawer grid-cols-[256px_1fr] '
+			: ' drawer grid-cols-[auto_1fr] '
+	}`;
 
-  const drawer_styles = `${
-    isSidebarOpen
-      ? 'drawer grid-cols-[256px_1fr] '
-      : ' drawer grid-cols-[auto_1fr] '
-  }`;
-
-  return (
-    <div className={drawer_styles}>
-      <input
-        id='my-drawer'
-        type='checkbox'
-        className='drawer-toggle'
-        onChange={handleSidebarToggle}
-      />
-      <div className='drawer-content-custom ml-3'>{children}</div>
-      <div className='drawer-side-custom'>
-        <ul
-          className={`menu ${
-            isSidebarOpen ? 'w-64' : 'w-28'
-          } bg-base-300 p-4 text-base-content`}
-        >
-          <label htmlFor='my-drawer' className='drawer-button btn'>
-            {isSidebarOpen && <span className='mr-2'>Filtros</span>}
-            <FaSlidersH size={'1rem'} color={'white'} />
-          </label>
-          {isSidebarOpen ? (
-            <>
-              <li className='pt-2'>
-                <div className='flex-col'>
-                  <div
-                    className='w-full '
-                    onClick={() =>
-                      setIsExpandedEstadoPropiedad(!isExpandedEstadoPropiedad)
-                    }
-                  >
-                    Estado de propiedad
-                    <FaChevronDown
-                      className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
-                        isExpandedEstadoPropiedad ? 'rotate-180' : ''
-                      }`}
-                    />
-                  </div>
-                  <div
-                    className={`w-full ${
-                      isExpandedEstadoPropiedad ? '' : 'hidden'
-                    }`}
-                  >
-                    <ul className='menu menu-compact'>
-                      <li className=''>
-                        <span className='flex-1'>Disponible</span>
-                      </li>
-                      <li className=''>
-                        <span className='flex-1'>Con ofertas</span>
-                      </li>
-                      <li className=''>
-                        <span className='flex-1'>En subasta</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </li>
-              <li className='pt-2'>
-                <div className='flex-col'>
-                  <div
-                    className='w-full '
-                    onClick={() => setIsExpandedPrecio(!isExpandedPrecio)}
-                  >
-                    Precio
-                    <FaChevronDown
-                      className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
-                        isExpandedPrecio ? 'rotate-180' : ''
-                      }`}
-                    />
-                  </div>
-                  <div className={`w-full grid gap-4 ${isExpandedPrecio ? '' : 'hidden'}`}>
-                    <PriceFilter />
-                  </div>
-                </div>
-              </li>
-              <li className='pt-2'>
-                <div className='flex-col'>
-                  <div
-                    className='w-full '
-                    onClick={() => setIsExpandedCategoria(!isExpandedCategoria)}
-                  >
-                    Categoría
-                    <FaChevronDown
-                      className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
-                        isExpandedCategoria ? 'rotate-180' : ''
-                      }`}
-                    />
-                  </div>
-                  <div
-                    className={`w-full ${isExpandedCategoria ? '' : 'hidden'}`}
-                  >
-                    <ul className='menu menu-compact'>
-                      <li className=''>
-                        <span className='flex-1'>Distrito</span>
-                      </li>
-                      <li className=''>
-                        <span className='flex-1'>Vecindario</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </li>
-            </>
-          ) : null}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className={drawer_styles}>
+			<input
+				id='my-drawer'
+				type='checkbox'
+				className='drawer-toggle'
+				onChange={handleSidebarToggle}
+			/>
+			<div className='drawer-content-custom ml-3'>{children}</div>
+			<div className='drawer-side-custom'>
+				<ul
+					className={`menu ${
+						isSidebarOpen ? 'w-64' : 'w-28'
+					} bg-base-300 p-4 text-base-content`}
+				>
+					<label htmlFor='my-drawer' className='drawer-button btn'>
+						{isSidebarOpen && <span className='mr-2'>Filtros</span>}
+						<FaSlidersH size={'1rem'} color={'white'} />
+					</label>
+					{isSidebarOpen ? (
+						<>
+							<li className='pt-2'>
+								<div className='flex-col'>
+									<div
+										className='w-full '
+										onClick={() =>
+											setIsExpandedEstadoPropiedad(!isExpandedEstadoPropiedad)
+										}
+									>
+										Estado de propiedad
+										<FaChevronDown
+											className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
+												isExpandedEstadoPropiedad ? 'rotate-180' : ''
+											}`}
+										/>
+									</div>
+									<div
+										className={`w-full ${
+											isExpandedEstadoPropiedad ? '' : 'hidden'
+										}`}
+									>
+										<ul className='menu menu-compact'>
+											<li className=''>
+												<span className='flex-1'>Disponible</span>
+											</li>
+											<li className=''>
+												<span className='flex-1'>Con ofertas</span>
+											</li>
+											<li className=''>
+												<span className='flex-1'>En subasta</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</li>
+							<li className='pt-2'>
+								<div className='flex-col'>
+									<div
+										className='w-full '
+										onClick={() => setIsExpandedPrecio(!isExpandedPrecio)}
+									>
+										Precio
+										<FaChevronDown
+											className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
+												isExpandedPrecio ? 'rotate-180' : ''
+											}`}
+										/>
+									</div>
+									<div
+										className={`grid w-full gap-4 ${
+											isExpandedPrecio ? '' : 'hidden'
+										}`}
+									>
+										<PriceFilter />
+									</div>
+								</div>
+							</li>
+							<li className='pt-2'>
+								<div className='flex-col'>
+									<div
+										className='w-full '
+										onClick={() => setIsExpandedCategoria(!isExpandedCategoria)}
+									>
+										Categoría
+										<FaChevronDown
+											className={`delay-400 float-right mt-1 h-5 w-5 transition-all duration-500 ${
+												isExpandedCategoria ? 'rotate-180' : ''
+											}`}
+										/>
+									</div>
+									<div
+										className={`w-full ${isExpandedCategoria ? '' : 'hidden'}`}
+									>
+										<ul className='menu menu-compact'>
+											<li className=''>
+												<span className='flex-1'>Distrito</span>
+											</li>
+											<li className=''>
+												<span className='flex-1'>Vecindario</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</li>
+						</>
+					) : null}
+				</ul>
+			</div>
+		</div>
+	);
 }
